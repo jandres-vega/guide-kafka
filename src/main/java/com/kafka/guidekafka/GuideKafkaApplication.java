5490122d3aa1ed5fa03bfc72101c732e95a95b1b
jorge.vega <jorge.vega@sofka.com.co>
2025-10-14 11:39:22 -0500

    first commit

---
+++ src/main/java/com/kafka/guidekafka/GuideKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.guidekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafka/guidekafka/GuideKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.guidekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
